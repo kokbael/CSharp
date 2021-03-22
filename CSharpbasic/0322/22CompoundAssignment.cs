08fe3354e51594360429c14f17c3a7deececde8f
kokbael <80227973+kokbael@users.noreply.github.com>
2021-03-22 10:04:06 +0900

    Add files via upload

---
+++ CSharpbasic/0322/22CompoundAssignment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace A024_CompoundAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 32;
+            Console.WriteLine(x += 2);
+            Console.WriteLine(x -= 8);
+            Console.WriteLine(x *= 3);
+            Console.WriteLine(x /= 2);
+            Console.WriteLine(x++);
+            Console.WriteLine(--x);
+        }
+    }
+}
